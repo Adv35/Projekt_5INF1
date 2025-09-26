c7af63246a44fa752d080c842703f7bd1c5f6ba9
adv35 <advik.vattamwar@gmail.com>
2025-09-26 18:30:58 +0200

    Password Handling completed in matter of Hashing and Salting. Both accomplished through the PasswordManagement class. Next task: Editing User class(es) to complete this setup.

--- src/main/java/com/adv/PasswordManagement.java
+++ src/main/java/com/adv/PasswordManagement.java
@@ -1,4 +1,66 @@
 package com.adv;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
 public class PasswordManagement {
+
+    private static final int SALT_BYTE_SIZE = 16;
+    private static final String HASH_ALGORITHM = "SHA-256";
+
+    private byte[] generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[SALT_BYTE_SIZE];
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    private String hashPasswordSub(String password, byte[] salt) {
+        try {
+            MessageDigest md = MessageDigest.getInstance(HASH_ALGORITHM);
+            md.update(salt);
+            md.update(password.getBytes(StandardCharsets.UTF_8));
+            byte[] hashedPassword = md.digest();
+
+            return Base64.getEncoder().encodeToString(hashedPassword);
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Cannot find following HashAlgorithm: " + HASH_ALGORITHM);
+        }
+
+    }
+
+    public String hashPassword(String password) {
+        byte[] salt = generateSalt();
+        String hashedPassword = hashPasswordSub(password, salt);
+        String saltString = Base64.getEncoder().encodeToString(salt);
+        return saltString + ":" + hashedPassword;
+    }
+
+    public boolean checkPassword(String password, String storedDbHash) {
+        if (storedDbHash == null || !storedDbHash.contains(":")) {
+            return false;
+        }
+        String[] parts = storedDbHash.split(":");
+        if (parts.length != 2) {
+            return false;
+        }
+
+        byte[] salt = Base64.getDecoder().decode(parts[0]);
+        String hashOfAttempt = hashPasswordSub(password, salt);
+        return hashOfAttempt.equals(parts[1]);
+    }
+
+
+//    public static void main(String[] args) {
+//        PasswordManagement pw = new PasswordManagement();
+//        String lol = pw.hashPassword("LOL");
+//        System.out.println(lol);
+//        System.out.println(pw.checkPassword("LOL", lol));
+//    }
+
+
 }
